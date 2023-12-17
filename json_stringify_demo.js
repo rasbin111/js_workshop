0595e45c2d3394322e048118a05a6122cb41b4b3
rasbin <rasbingodarthapa@gmail.com>
2023-12-17 19:03:07 +0545

    simple demo of JSON.stringify()

---
+++ json_stringify_demo.js
@@ -0,0 +1,8 @@
+const person = {
+    name: "Ramk",
+    age: 57,
+    address: "Bhimeshwor - 05, Mati, Dolakha"
+}
+
+const person_json = JSON.stringify(person); // convert js object into json
+console.log(person_json);
